1c71f176334c7da824a45923551a987c0027dcaf
rehaan redkar <111688528+niklaus25@users.noreply.github.com>
2022-08-23 19:27:30 +0530

    doc - Adding ' in the comment

--- src/v1/router/index.js
+++ src/v1/router/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const rateLimit = require('express-rate-limit');
 const { verifyFCMToken } = require('../../../middleware/authMiddleware');
 
-// adding rate limit for the routes so that unwanted load of requests doesnt harm server
+// adding rate limit for the routes so that unwanted load of requests doesn't harm server
 const limit = rateLimit({
   max: 100, // max requests
   windowMs: 60 * 60 * 1000, // 1 Hour
