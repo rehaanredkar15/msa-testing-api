f5183476b958ca257cd282ad730482a87dcba7d0
rehaan redkar <rehaanredkar92@gmail.com>
2022-11-03 21:30:30 +0530

    new api

--- src/v1/controllers/AuthController.js
+++ src/v1/controllers/AuthController.js
@@ -1,5 +1,7 @@
 const { signClientAccessToken, signClientRefreshToken } = require('../../../helpers/jwt_helper');
 const { verifyClientRefreshToken } = require('../../../middleware/authMiddleware');
+const axios = require('axios');
+
 
 exports.ClientRefreshingToken = async (req, res) => {
   const { refreshToken } = req.body;
@@ -15,5 +17,84 @@ exports.ClientRefreshingToken = async (req, res) => {
     res.send({ accessToken, refreshToken: refToken });
   } catch (err) {
     res.status(500).json(err);
+  }
+};
+
+exports.GetLocationDetailsFromIp = async (req, res) => {
+  const { ip } = req.body;
+
+  if (!ip) {
+   return res.status(200).json('Please provide an ip address of the user');
+  }
+ 
+  let apiResponse ;
+  let usersDetails = [];
+
+
+  try {
+    
+    let url = ` http://www.geoplugin.net/json.gp?ip=${ip}`;
+    
+    axios({
+      method:'get',
+      url,
+  })
+  .then(function (response) {
+ 
+    apiResponse = JSON.stringify(response.data);
+
+     usersDetails.push({ city : response.data.geoplugin_city })
+     usersDetails.push({ latitude : parseFloat(response.data.geoplugin_latitude) })
+     usersDetails.push({ longitude : parseFloat(response.data.geoplugin_longitude) })
+
+     return res.status(200).json({  
+      success: true,
+      count:0,
+      message: 'Users Location details found',
+      data:usersDetails 
+    });
+  })
+  .catch(function (error) {
+ 
+    let url = `http://api.ipapi.com/${ip}?access_key=${process.env.IPAPI_ACCESS_KEY}`;
+   
+    axios({
+      method:'get',
+      url,
+   })
+  .then(function (response) {
+
+    apiResponse = JSON.stringify(response.data);
+
+     usersDetails.push({ city : response.data.city })
+     usersDetails.push({ latitude : response.data.latitude })
+     usersDetails.push({ longitude : response.data.longitude })
+
+     return res.status(200).json({  
+      success: true,
+      count:0,
+      message: 'Users Location details found',
+      data:usersDetails 
+    });
+  })
+  .catch(function (error) {
+
+        return res.status(500).json({
+          success: false,
+          count: 0 ,
+          message: " Failed to Fetch user's location " + error.message, 
+          data: []
+          })
+      });
+  });
+
+  } catch (err) {
+    return res.status(500).json({
+
+      success: false,
+      count: 0 ,
+      message: " Failed to Fetch user's location" + error.message, 
+      data: []
+      })
   }
 };
--- src/v1/router/api/AuthApi.js
+++ src/v1/router/api/AuthApi.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const {
-  ClientRefreshingToken,
+  ClientRefreshingToken, GetLocationDetailsFromIp
 } = require('../../controllers/AuthController');
 
 const AuthApi = new Router();
 
 // Get the client token refreshed
 AuthApi.post('/clientRefresh', ClientRefreshingToken);
+
+// Get the client token refreshed
+AuthApi.post('/getLocationDetailsFromIp', GetLocationDetailsFromIp);
 
 module.exports = AuthApi;
